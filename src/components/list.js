7802968d75524f0714e97577af7e1bc326c2a050
nadeeraka <nimanthanadeeraka@outlook.com>
2019-03-14 16:45:59 +0530

    V 0.0.3

--- src/components/list.js
+++ src/components/list.js
@@ -1,10 +1,11 @@
 import React from "react";
 import Item from "./Item";
+import uuid from "uuid";
 const List = props => (
   <>
     {props.todos.map(res => {
       return (
-        <li key={res}>
+        <li key={uuid()}>
           {" "}
           <Item data={res} />{" "}
         </li>
