04cfaf888d87043a68615deaf99bc1c13b514cb4
nadeeraka <nimanthanadeeraka@outlook.com>
2019-03-15 11:01:54 +0530

    V 0.0.6

--- src/components/title.js
+++ src/components/title.js
@@ -2,7 +2,7 @@ import React from "react";
 const Title = () => {
   return (
     <div>
-      <nav className="navbar navbar-dark  bg-warning pup">
+      <nav className="navbar  navbar-expand-lg navbar-dark  bg-warning pup">
         <span className="navbar-brand mb-0 h1 mx-auto font2">
           <h2 className="cd nn">To-Do List </h2>
           <h3> simple tool for organize your things</h3>
